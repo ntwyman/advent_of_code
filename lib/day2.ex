51fe13f9a59c9eda4905c8eff190a61f9b5c8f64
Nick Twyman <nick@truss.works>
2019-12-06 14:37:46 -0800

    Added ElixirLS to vscode and allowed it to reformat the code

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,68 +1,71 @@
 defmodule Day2 do
-    defp do_op(state, index, op) do
-        arg1 = :array.get(:array.get(index+1, state), state)
-        arg2 = :array.get(:array.get(index+2, state), state)
-        res_pos = :array.get(index+3, state)
-        :array.set(res_pos, op.(arg1, arg2), state)
-    end
+  defp do_op(state, index, op) do
+    arg1 = :array.get(:array.get(index + 1, state), state)
+    arg2 = :array.get(:array.get(index + 2, state), state)
+    res_pos = :array.get(index + 3, state)
+    :array.set(res_pos, op.(arg1, arg2), state)
+  end
 
-    defp run_from(state, index) do
-        case :array.get(index,state) do
-            1 -> run_from(do_op(state, index, &Kernel.+/2), index + 4)
-            2 -> run_from(do_op(state, index, &Kernel.*/2), index + 4)
-            99 -> state
-        end
+  defp run_from(state, index) do
+    case :array.get(index, state) do
+      1 -> run_from(do_op(state, index, &Kernel.+/2), index + 4)
+      2 -> run_from(do_op(state, index, &Kernel.*/2), index + 4)
+      99 -> state
     end
+  end
 
-    defp load_program(file) do
-        File.read!(file)
-        |> String.trim()
-        |> String.split(",")
-        |> Enum.map(&String.to_integer/1)
-    end
+  defp load_program(file) do
+    File.read!(file)
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
 
-    def run(state) do
-        :array.from_list(state)
-        |> run_from(0)
-        |> :array.to_list()
-    end
+  def run(state) do
+    :array.from_list(state)
+    |> run_from(0)
+    |> :array.to_list()
+  end
 
-    defp execute([op1, _addr1, _addr2 | rest], noun, verb) do
-        run([ op1, noun, verb ] ++ rest)
-        |> hd
-    end
+  defp execute([op1, _addr1, _addr2 | rest], noun, verb) do
+    run([op1, noun, verb] ++ rest)
+    |> hd
+  end
 
-    defp find_noun_and_verb(_program, _target, _noun, verb) when verb >= 100, do: 100
-    defp find_noun_and_verb(program, target, noun, verb) do
-        if execute(program, noun, verb) == target do
-            verb
-        else
-            find_noun_and_verb(program, target, noun, verb + 1)
-        end
-    end
-   
-    defp find_noun_and_verb(_program, _target, noun) when noun >= 100, do: {100, 100} 
-    defp find_noun_and_verb(program, target, noun) do
-        verb = find_noun_and_verb(program, target, noun, 0)
-        if verb < 100 do
-            { noun, verb }
-        else
-            find_noun_and_verb(program, target, noun + 1)
-        end
-    end
+  defp find_noun_and_verb(_program, _target, _noun, verb) when verb >= 100, do: 100
 
-    defp find_noun_and_verb(program, target) do
-        {noun, verb} = find_noun_and_verb(program, target, 0)
-        noun * 100 + verb
+  defp find_noun_and_verb(program, target, noun, verb) do
+    if execute(program, noun, verb) == target do
+      verb
+    else
+      find_noun_and_verb(program, target, noun, verb + 1)
     end
+  end
 
-     def part1(file) do
-        load_program(file)
-        |> execute(12, 2)
-    end
+  defp find_noun_and_verb(_program, _target, noun) when noun >= 100, do: {100, 100}
 
-    def part2(file) do
-        load_program(file)
-        |> find_noun_and_verb(19690720)
+  defp find_noun_and_verb(program, target, noun) do
+    verb = find_noun_and_verb(program, target, noun, 0)
+
+    if verb < 100 do
+      {noun, verb}
+    else
+      find_noun_and_verb(program, target, noun + 1)
     end
+  end
+
+  defp find_noun_and_verb(program, target) do
+    {noun, verb} = find_noun_and_verb(program, target, 0)
+    noun * 100 + verb
+  end
+
+  def part1(file) do
+    load_program(file)
+    |> execute(12, 2)
+  end
+
+  def part2(file) do
+    load_program(file)
+    |> find_noun_and_verb(19_690_720)
+  end
 end
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -3,14 +3,12 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-
   test "Day3 Part1 segment" do
-    assert Day3.segment({0, 0}, "R4") == {{4, 0}, {0, 0, 4,0}}
-    assert Day3.segment({15, 23}, "D2") == {{15,21}, {15, 21, 15, 23}}
+    assert Day3.segment({0, 0}, "R4") == {{4, 0}, {0, 0, 4, 0}}
+    assert Day3.segment({15, 23}, "D2") == {{15, 21}, {15, 21, 15, 23}}
   end
 
   test "Day3 Part1 segments" do
     assert Day3.segments(["R4", "D3"], {0, 0}) == [{0, 0, 4, 0}, {4, -3, 4, 0}]
   end
-
 end
