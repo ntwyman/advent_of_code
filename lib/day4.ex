6d4308fa55959447045136f16d5b2ab3d4ef6227
Nick Twyman <nick@truss.works>
2019-12-07 14:33:01 -0800

    4.2

--- lib/day4.ex
+++ lib/day4.ex
@@ -15,18 +15,51 @@ defmodule Day4 do
     end
   end
 
-  @spec is_valid([0..9]) :: boolean
-  def is_valid(digits) do
+  @spec has_clean_repeat([0..9]) :: boolean
+  defp has_clean_repeat(digits) do
+    {_, sequence_length, double_seen} =
+      Enum.reduce(digits, {99, 0, false}, fn digit, {last_seen, sequence_length, double_seen} ->
+        if digit == last_seen do
+          {digit, sequence_length + 1, double_seen}
+        else
+          {digit, 1, sequence_length == 2 || double_seen}
+        end
+      end)
+
+    double_seen || sequence_length == 2
+  end
+
+  @spec digits_down(non_neg_integer) :: [0..9]
+  def digits_down(num) when num == 0, do: []
+
+  def digits_down(num) do
+    [rem(num, 10) | digits_down(div(num, 10))]
+  end
+
+  @spec digits(non_neg_integer) :: [0..9]
+  def digits(num) do
+    Enum.reverse(digits_down(num))
+  end
+
+  @spec is_valid(integer) :: boolean
+  def is_valid(num) do
+    digits = digits(num)
     monotonic(digits) and has_repeat(digits)
   end
 
-  @spec is_valid_string(String.t()) :: boolean
-  def is_valid_string(pwd) do
-    is_valid(for c <- to_charlist(pwd), do: c - ?0)
+  @spec is_valid_2(integer) :: boolean
+  def is_valid_2(num) do
+    digits = digits(num)
+    monotonic(digits) and has_clean_repeat(digits)
   end
 
   @spec part1(String.t()) :: integer
   def part1(_file_name) do
-    Enum.count(125_730..579_381, fn num -> is_valid_string(Integer.to_string(num)) end)
+    Enum.count(125_730..579_381, &is_valid/1)
+  end
+
+  @spec part2(String.t()) :: integer
+  def part2(_file_name) do
+    Enum.count(125_730..579_381, &is_valid_2/1)
   end
 end
--- test/day4_test.exs
+++ test/day4_test.exs
@@ -3,8 +3,15 @@ defmodule Day4Test do
   doctest Day4
 
   test "Day4 Part1 is_valid" do
-    assert Day4.is_valid_string("111111")
-    assert not Day4.is_valid_string("223450")
-    assert Day4.is_valid_string("1123789")
+    assert Day4.is_valid(111_111)
+    assert not Day4.is_valid(223_450)
+    assert not Day4.is_valid(123_789)
+  end
+
+  test "Day4 Part2 is_valid_2" do
+    assert Day4.is_valid_2(112_233)
+    assert not Day4.is_valid_2(123_444)
+    assert Day4.is_valid_2(111_122)
+    assert Day4.is_valid_2(127_789)
   end
 end
