f0438bbc2328cf31b7085b64a5c83c44c151f8bc
Nick Twyman <nick@truss.works>
2019-12-08 10:30:39 -0800

    8.2

--- lib/day8.ex
+++ lib/day8.ex
@@ -17,28 +17,58 @@ defmodule Day8 do
     )
   end
 
+  @spec layer_cake(String.t()) :: [String.t()]
+  def layer_cake(file_name) do
+    File.read!(file_name)
+    |> String.trim()
+    |> make_splits(150)
+  end
+
   @spec part1(String.t()) :: integer
   def part1(file_name) do
-    layer_counts =
-      File.read!(file_name)
-      |> String.trim()
-      |> make_splits(150)
-      |> Enum.map(&count_chars/1)
+    layer_counts = Enum.map(layer_cake(file_name), &count_chars/1)
 
     {_, c} =
       Enum.reduce(layer_counts, {150, 0}, fn cnts, {zcnt, mult} ->
         zcount = cnts[?0]
-        IO.puts(zcount)
 
         if not is_nil(zcount) and zcount < zcnt do
-          IO.puts("switch")
           {zcount, cnts[?1] * cnts[?2]}
         else
-          IO.puts("stick")
           {zcnt, mult}
         end
       end)
 
     c
+  end
+
+  @spec decode(char()) :: char()
+  def decode(pixel_code) do
+    case pixel_code do
+      ?0 -> 0x20
+      ?1 -> ?*
+      _ -> pixel_code
+    end
+  end
+
+  @spec opacity(charlist(), charlist()) :: charlist()
+  def opacity([], []), do: []
+
+  def opacity(behind, front) do
+    [b_1 | b_rest] = behind
+    [f_1 | f_rest] = front
+    v = decode(if f_1 == ?2, do: b_1, else: f_1)
+    [v | opacity(b_rest, f_rest)]
+  end
+
+  @spec part2(String.t()) :: String.t()
+  def part2(file_name) do
+    layers =
+      layer_cake(file_name)
+      |> Enum.map(&to_charlist/1)
+
+    image = to_string(Enum.reduce(layers, &opacity/2))
+
+    Enum.join(make_splits(image, 25), "\n")
   end
 end
