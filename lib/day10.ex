c1e15a3010da011d46037d9e84f52a498e2e6bf4
Nick Twyman <nick@truss.works>
2019-12-10 23:36:54 -0500

    10.2

--- lib/day10.ex
+++ lib/day10.ex
@@ -3,38 +3,19 @@ defmodule Day10 do
     defstruct x: 0, y: 0
   end
 
-  @spec angle(integer, integer) :: String.t()
-  defp angle(delta_x, delta_y) do
-    x_mag = abs(delta_x)
-    y_mag = abs(delta_y)
-    x_sign = if delta_x < 0, do: "-", else: ""
-    y_sign = if delta_y < 0, do: "-", else: ""
-
-    {x_ang, y_ang} =
-      case {x_mag, y_mag} do
-        {0, _} ->
-          {0, 1}
-
-        {_, 0} ->
-          {1, 0}
-
-        _ ->
-          d = Integer.gcd(x_mag, y_mag)
-          {x_mag / d, y_mag / d}
-      end
-
-    "#{x_sign}#{x_ang}:#{y_sign}#{y_ang}"
+  @spec angle(Pos.t(), Pos.t()) :: float
+  def angle(base, pos) do
+    # Y goes down and measuring angles clockwise from UP. Hence odd delta's
+    angle = ElixirMath.atan2(pos.x - base.x / 1, base.y - pos.y / 1)
+    if angle < 0, do: angle + 2 * :math.pi(), else: angle
   end
 
   defp count_visible(positions, base) do
     Enum.reduce(positions, MapSet.new(), fn pos, angles ->
-      delta_x = pos.x - base.x
-      delta_y = pos.y - base.y
-
-      if delta_x == 0 and delta_y == 0 do
+      if pos == base do
         angles
       else
-        MapSet.put(angles, angle(delta_x, delta_y))
+        MapSet.put(angles, angle(base, pos))
       end
     end)
     |> MapSet.size()
@@ -57,14 +38,12 @@ defmodule Day10 do
   defp line_to_positions({line, y_coord}) do
     to_charlist(line)
     |> Enum.with_index()
-    |> Enum.map(fn {chr, x_coord} ->
-      if chr == ?#, do: %Pos{x: x_coord, y: y_coord}, else: %Pos{x: -1, y: -1}
-    end)
-    |> Enum.reject(fn pos -> pos == %Pos{x: -1, y: -1} end)
+    |> Enum.filter(fn {chr, _} -> chr == ?# end)
+    |> Enum.map(fn {_, x_coord} -> %Pos{x: x_coord, y: y_coord} end)
   end
 
-  @spec reduce_map([String.t()]) :: [Pos.t()]
-  def reduce_map(space_map) do
+  @spec reduce_to_map([String.t()]) :: [Pos.t()]
+  def reduce_to_map(space_map) do
     Enum.with_index(space_map)
     |> Enum.map(&line_to_positions/1)
     |> List.flatten()
@@ -72,7 +51,7 @@ defmodule Day10 do
 
   @spec search_map([String.t()]) :: {integer, Pos.t()}
   def search_map(space_map) do
-    search(reduce_map(space_map))
+    search(reduce_to_map(space_map))
   end
 
   @spec part1(String.t()) :: String.t()
@@ -82,5 +61,83 @@ defmodule Day10 do
       |> search_map()
 
     "Base at #{base.x}, #{base.y}, can see #{best} asteroids"
+  end
+
+  @type a_vec :: {Pos.t(), integer}
+  @spec insert_in_list([a_vec()], a_vec()) :: [a_vec()]
+  def insert_in_list(boom_list, target) do
+    case boom_list do
+      [] ->
+        [target]
+
+      [nearest | hidden] ->
+        if elem(target, 1) < elem(nearest, 1) do
+          [target | boom_list]
+        else
+          [nearest | insert_in_list(hidden, target)]
+        end
+    end
+  end
+
+  @spec extract_booms([[a_vec()]]) :: [Pos.t()]
+  defp extract_booms(angle_lists) do
+    # Each item in the input is a non-empty list of the asteroids along a given vector.
+    # The lists are in angular order, so each time round the laser booms the first one
+    # on the list
+
+    case angle_lists do
+      [] ->
+        []
+
+      # Can do this nested as we know lists are non-empty
+      [[boomer | saved_for_now] | other_vectors] ->
+        new_list =
+          if saved_for_now == [], do: other_vectors, else: other_vectors ++ [saved_for_now]
+
+        [elem(boomer, 0) | extract_booms(new_list)]
+    end
+  end
+
+  @spec boom_list([Pos.t()], Pos.t()) :: [Pos.t()]
+  def boom_list(positions, base) do
+    boom_hash =
+      Enum.reduce(positions, %{}, fn pos, order_hash ->
+        if base == pos do
+          order_hash
+        else
+          angle_key = angle(base, pos)
+          old_list = Map.get(order_hash, angle_key, [])
+
+          Map.put(
+            order_hash,
+            angle_key,
+            insert_in_list(old_list, {pos, abs(pos.x - base.x) + abs(pos.y - base.y)})
+          )
+        end
+      end)
+
+    Map.keys(boom_hash)
+    |> Enum.sort()
+    |> Enum.map(fn line_of_sight -> boom_hash[line_of_sight] end)
+    |> extract_booms()
+  end
+
+  @spec boom_order([String.t()]) :: [Pos.t()]
+  def boom_order(lines) do
+    positions = reduce_to_map(lines)
+
+    {_, base} = search(positions)
+    boom_list(positions, base)
+  end
+
+  @spec part2(String.t()) :: integer
+  def part2(file_name) do
+    pos =
+      Files.read_lines!(file_name)
+      |> boom_order()
+      # Two hard things in computing
+      |> Enum.at(199)
+
+    pos.x * 100 + pos.y
   end
 end
--- test/day10_test.exs
+++ test/day10_test.exs
@@ -3,6 +3,17 @@ defmodule Day10Test do
   use ExUnit.Case
   doctest Day10
 
+  test "Day 10 Angle" do
+    # Up
+    assert Day10.angle(%Pos{x: 1, y: 1}, %Pos{x: 1, y: 0}) == 0.0
+    # right
+    assert Day10.angle(%Pos{x: 1, y: 1}, %Pos{x: 2, y: 1}) == :math.pi() / 2
+    # down
+    assert Day10.angle(%Pos{x: 1, y: 1}, %Pos{x: 1, y: 2}) == :math.pi()
+    # left
+    assert Day10.angle(%Pos{x: 1, y: 1}, %Pos{x: 0, y: 1}) == 3 * :math.pi() / 2.0
+  end
+
   @small [".#..#", ".....", "#####", "....#", "...##"]
 
   @example_1 [
@@ -19,7 +30,7 @@ defmodule Day10Test do
   ]
 
   test "Day 10 Part1" do
-    assert Day10.reduce_map(@small) == [
+    assert Day10.reduce_to_map(@small) == [
              %Pos{x: 1, y: 0},
              %Pos{x: 4, y: 0},
              %Pos{x: 0, y: 2},
@@ -33,5 +44,64 @@ defmodule Day10Test do
            ]
 
     assert Day10.search_map(@example_1) == {33, %Pos{x: 5, y: 8}}
+  end
+
+  @small_boom [
+    ".#....#####...#..",
+    "##...##.#####..##",
+    "##...#...#.#####.",
+    "..#.....#...###..",
+    "..#.#.....#....##"
+  ]
+
+  test "Day 10, Small Boom" do
+    positions = Day10.reduce_to_map(@small_boom)
+
+    assert Enum.take(Day10.boom_list(positions, %Pos{x: 8, y: 3}), 9) ==
+             [
+               %Pos{x: 8, y: 1},
+               %Pos{x: 9, y: 0},
+               %Pos{x: 9, y: 1},
+               %Pos{x: 10, y: 0},
+               %Pos{x: 9, y: 2},
+               %Pos{x: 11, y: 1},
+               %Pos{x: 12, y: 1},
+               %Pos{x: 11, y: 2},
+               %Pos{x: 15, y: 1}
+             ]
+  end
+
+  @boom_test [
+    ".#..##.###...#######",
+    "##.############..##.",
+    ".#.######.########.#",
+    ".###.#######.####.#.",
+    "#####.##.#.##.###.##",
+    "..#####..#.#########",
+    "####################",
+    "#.####....###.#.#.##",
+    "##.#################",
+    "#####.##.###..####..",
+    "..######..##.#######",
+    "####.##.####...##..#",
+    ".#####..#.######.###",
+    "##...#.##########...",
+    "#.##########.#######",
+    ".####.#.###.###.#.##",
+    "....##.##.###..#####",
+    ".#.#.###########.###",
+    "#.#.#.#####.####.###",
+    "###.##.####.##.#..##"
+  ]
+  test "Day 10 Part2" do
+    big_boom_list = Day10.boom_order(@boom_test)
+
+    assert Enum.take(big_boom_list, 3) == [
+             %Pos{x: 11, y: 12},
+             %Pos{x: 12, y: 1},
+             %Pos{x: 12, y: 2}
+           ]
+
+    assert Enum.at(big_boom_list, 99) == %Pos{x: 10, y: 16}
   end
 end
