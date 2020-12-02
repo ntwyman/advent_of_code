26fd93729d36b5a33eb58735cadf728797f26903
Nick Twyman <nick@truss.works>
2020-12-01 17:07:49 -0800

    1.1

---
+++ cli/cli.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System.IO
+
+let rec find2020Pair numbers =
+    match numbers with
+    | a :: rest ->
+        try
+            (a, List.find (fun b -> a + b = 2020) rest)
+        with
+            | :? System.Collections.Generic.KeyNotFoundException -> find2020Pair rest
+    | _ -> (0, 0)
+
+
+[<EntryPoint>]  
+let main argv =
+    let lines = File.ReadAllLines(@"inputs/day1.txt")
+    let numbers = Seq.map int lines |> Seq.toList
+    let (a, b) = find2020Pair numbers
+    printfn "%d, %d" a b 
+    0 // return an integer exit code
