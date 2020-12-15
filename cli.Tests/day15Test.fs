c74b7ea8aac66d2010e8e64153ddc6add94a079f
Nick Twyman <nick@conky.org>
2020-12-15 11:28:50 -0800

    Day 15 WIP

---
+++ cli.Tests/day15Test.fs
@@ -0,0 +1,18 @@
+﻿module Day15.Tests
+
+open NUnit.Framework
+open AoC
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let shoutOuts () =
+    Assert.AreEqual(436, Day15.whatComes2020th [0;3;6])
+    Assert.AreEqual(1, Day15.whatComes2020th [1;3;2])
+    Assert.AreEqual(10, Day15.whatComes2020th [2;1;3])
+    Assert.AreEqual(27, Day15.whatComes2020th [1;2;3])
+    Assert.AreEqual(78, Day15.whatComes2020th [2;3;1])
+    Assert.AreEqual(438, Day15.whatComes2020th [3;2;1])
+    Assert.AreEqual(1836, Day15.whatComes2020th [3;1;2])
---
+++ cli/day15.fs
@@ -0,0 +1,23 @@
+﻿namespace AoC
+
+module Day15 =
+
+
+    let whatComes2020th numbers =
+        let initialNums = Seq.indexed numbers
+        let primePump (lastSeen, turnMap:Map<int, int>) (idx, num) =
+            printfn "%d - %d" (idx + 1) num
+            (num, turnMap.Add(num, idx + 1))
+        let lastSeen, turnMap = Seq.fold primePump (0, Map.empty) initialNums
+        let rec takeTurns turn lastSeen turnMap = 
+            let lastTime = Map.tryFind lastSeen turnMap
+            let shout = match lastTime with
+                        | Some (t) -> turn - t
+                        | _ -> 0
+            printfn "%d - %d" turn shout
+
+            if turn = 2020 then
+                shout
+            else
+                takeTurns (turn + 1) shout (turnMap.Add(shout, turn))
+        takeTurns (1 + Seq.length initialNums) lastSeen turnMap
