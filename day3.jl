0705205b9541eb03abf1e68057c582e916273838
Nick Twyman <nick@conky.org>
2021-12-02 21:35:24 -0800

    Day3 Part1

--- day3.jl
+++ day3.jl
@@ -8,23 +8,42 @@ s=ArgParseSettings()
 end
 parsed_args = parse_args(ARGS, s)
 
-function part1(moves)
-    "Not implemented yet"
+function part1(bits)
+    LENGTH = 12
+    counts = zeros(Int32, LENGTH)
+    lc = 0
+    for b in bits
+        for idx in 1:LENGTH
+            if b[idx] == "1"
+                counts[idx] += 1
+            end
+        end
+        lc += 1
+    end
+    lc = div(lc, 2)
+    gamma::UInt16 = 0
+    for idx in 1:LENGTH
+        gamma <<= 1
+        if counts[idx] > lc
+            gamma |= 1
+        end
+    end
+    epsilon = xor(0x0fff,gamma)
+    UInt32(epsilon) * UInt32(gamma)
 end
 
-function part2(moves)
+function part2(bits)
     "Not implemented yet"
 end
 
 function parseLine(line)
-    parts = split(line)
-    (parts[1], parse(Int32, parts[2]))
+    split(line,"")
 end
 
-moves = [parseLine(line) for line in eachline("input/day_2.txt")]
+bits = [parseLine(line) for line in eachline("input/day_3.txt")]
 if parsed_args["part2"]
-    answer = part2(moves)
+    answer = part2(bits)
 else
-    answer = part1(moves)
+    answer = part1(bits)
 end
 println("The answer is: $answer")
