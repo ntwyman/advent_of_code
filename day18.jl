71c44705aeedff6e68e53eb6c41c7a1b0c9759b7
Nick Twyman <nick@conky.org>
2021-12-17 19:28:46 -0800

    Day18 Part0

---
+++ day18.jl
@@ -0,0 +1,26 @@
+using ArgParse
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--part2"
+    help = "Do part 2"
+    action = :store_true
+    "--test"
+    help = "Run test data"
+    action = :store_true
+end
+parsed_args = parse_args(ARGS, s)
+
+test_suffix = parsed_args["test"] ? "_test" : ""
+input = readlines("input/day_18$test_suffix.txt")
+
+function part1()
+    "No one"
+end
+
+function part2()
+    "No two"
+end
+
+p = parsed_args["part2"] ? part2 : part1
+answer = p()
+println("The answer is: $answer")
